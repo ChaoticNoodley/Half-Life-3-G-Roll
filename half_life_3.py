cedf5f030b18701b4d85eb562684025ae33f454f
Bruno Ceroni <66728765+ChaoticNoodley@users.noreply.github.com>
2026-02-21 21:42:42 -0300

    arrumando todo o codigo para fazer o "g-roll" funcionar

--- half_life_3.py
+++ half_life_3.py
@@ -1,10 +1,19 @@
-import sys, os
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel
-from PyQt6.QtCore import Qt, QTimer, QUrl
-from PyQt6.QtGui import QIcon, QPixmap
-from PyQt6.QtMultimedia import QMediaPlayer, QAudioOutput
-from PyQt6.QtMultimediaWidgets import QVideoWidget
+import os
+import sys
 
+# force locale
+os.environ["LC_NUMERIC"] = "C"
+import locale
+try:
+    locale.setlocale(locale.LC_NUMERIC, 'C')
+except:
+    pass
+
+# otters imports
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel
+from PyQt6.QtCore import Qt, QTimer
+from PyQt6.QtGui import QIcon, QPixmap
+import mpv
 
 def resource_path(relative_path):
     try:
@@ -12,7 +21,6 @@ def resource_path(relative_path):
     except Exception:
         base_path = os.path.abspath(".")
     return os.path.join(base_path, relative_path)
-
 
 app = QApplication(sys.argv)
 
@@ -28,21 +36,21 @@ background.setPixmap(pixmap)
 background.setScaledContents(True)
 background.resize(600, 800)
 
-text = QLabel("Configuring sudo panda...\nPlease wait", window)
+text = QLabel("Compiling Shaders...\nPlease wait", window)
 text.setStyleSheet("color: white; font-size: 18px;")
 text.setAlignment(Qt.AlignmentFlag.AlignCenter)
 text.setGeometry(0, 650, 600, 100)
 
-video_widget = QVideoWidget(window)
-video_widget.setGeometry(0, 0, 1280, 720)
-video_widget.hide()
+# make windows video (QWidget)
+video_container = QWidget(window)
+video_container.setGeometry(0, 0, 1280, 720)
+video_container.hide()
 
-audio = QAudioOutput()
-player = QMediaPlayer()
-player.setAudioOutput(audio)
+# MPV config
+player = mpv.MPV(wid=str(int(video_container.winId())))
+
 
 window.show()
-
 
 def start_video():
     background.hide()
@@ -50,28 +58,18 @@ def start_video():
 
     window.setFixedSize(1280, 720)
     screen = app.primaryScreen().size()
-    window.move(
-        (screen.width() - 1280) // 2,
-        (screen.height() - 720) // 2
-    )
+    window.move((screen.width() - 1280) // 2, (screen.height() - 720) // 2)
 
-    video_widget.setGeometry(0, 0, 1280, 720)
-    video_widget.show()
+    video_container.show()
+    
+    video_path = resource_path("rickroll.webm")
+    player.play(video_path)
 
-    player.setVideoOutput(video_widget)
-
-    video_path = resource_path("rickroll.mp4")
-    player.setSource(QUrl.fromLocalFile(video_path))
-    audio.setVolume(1.0)
-    player.play()
-
-
-def on_media_status(status):
-    if status == QMediaPlayer.MediaStatus.EndOfMedia:
+# Fechar o app quando o vídeo acabar
+@player.property_observer('eof-reached')
+def on_end(name, value):
+    if value:
         app.quit()
-
-
-player.mediaStatusChanged.connect(on_media_status)
 
 QTimer.singleShot(3000, start_video)
 
